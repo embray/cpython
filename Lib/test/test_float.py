56435abc8fbbbc3a998cc21906d28f48e93b42c3
Erik M. Bray <erik.bray@lri.fr>
2017-05-22 10:57:52 +0200

    Workaround to UnicodeDecodeError when calling locale.localeconv() after setting LC_NUMERIC to fr_FR.  This causes mbstowcs to fail unless we also set LC_CTYPE to fr_FR to set the appropriate code page to use.
    It could be said that just setting LC_NUMERIC without setting an
    appropriate LC_CTYPE is not a valid locale setting.

--- Lib/test/test_float.py
+++ Lib/test/test_float.py
@@ -142,6 +142,7 @@ class GeneralFloatCases(unittest.TestCase):
         check(b'123\xa0')
 
     @support.run_with_locale('LC_NUMERIC', 'fr_FR', 'de_DE')
+    @support.run_with_locale('LC_CTYPE', 'fr_FR', 'de_DE')
     def test_float_with_comma(self):
         # set locale to something that doesn't use '.' for the decimal point
         # float must not accept the locale specific decimal point but
